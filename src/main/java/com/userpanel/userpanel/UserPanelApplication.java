0b0be280c0a6f9441c17029a35a48d36b6831c99
romantulchak <romantulchak29@gmail.com>
2023-01-16 21:07:21 +0300

    Init project

---
+++ src/main/java/com/userpanel/userpanel/UserPanelApplication.java
@@ -0,0 +1,13 @@
+package com.userpanel.userpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPanelApplication.class, args);
+	}
+
+}
